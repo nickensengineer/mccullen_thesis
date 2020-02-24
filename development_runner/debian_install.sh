fe5be574c43924ebe8afc6ff379d15643e4173f1
zachary.nickens <zachary.nickens@woolpert.com>
2020-02-24 08:29:10 -0600

    debugging the use of numpy modules in the switch from py2 to py3

---
+++ development_runner/debian_install.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo apt-get install curl libgl1-mesa-glx libegl1-mesa libxrandr2 libxrandr2 libxss1 libxcursor1 libxcomposite1 libasound2 libxi6 libxtst6 - y
+
+curl -O https://repo.anaconda.com/archive/Anaconda3-2019.10-Linux-x86_64.sh
+
